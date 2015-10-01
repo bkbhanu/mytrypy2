fd46cfcc89008c707019666eeb2cec8bf93b2d91
Bhanu Prasad <Bhanu@Bhanu.local>
2015-10-01 10:42:19 -0400

    commitagain

--- webinteraction.py
+++ webinteraction.py
@@ -1,4 +1,4 @@
-import urllib
+﻿import urllib
 import os
 import re
 import json
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 
 test 10:07
 test 10:08
- test msg from mac
+ test msg from mac how is this going from windows
  dsfdf
  adsafdsf
+
+ I still cant see why it works perfectly from vs2015, what !
